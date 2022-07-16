3dec5c2e8195465a0ee0272522e9db46ab1efe9c
Pukar Chhatkuli <ckrp2018@gmail.com>
2022-07-16 16:28:27 +0545

    Product routes and controllers

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -1,6 +1,56 @@
+const ProductService = require("../services/product.service");
+const slugify = require("slugify");
+const ProductModel = require("../models/product.model");
 class ProductController{
+    constructor(){
+        this.prod_svc = new ProductService();
+    }
     addProduct = (req, res, next) => {
-        // TODO: Add product
+        // TODO: Add Product
+        try{
+            let data = req.body;
+            if (req.files){
+                let images = [];
+                req.files.map((image) => {
+                    images.push(image)
+                })
+                data.images = images;
+            }
+
+            this.prod_svc.validateProduct(data);
+
+            data.slug = slugify(data.title.toLowerCase());
+            data.after_discount = data.price - data.price * data.discount / 100;
+
+            if (!data.category){
+                data.category = null;
+            }
+            if (!data.brands){
+                data.brands = null;
+            }
+            if (!data.seller){
+                data.seller = null;
+            }
+
+            let product = new ProductModel(data);
+            product.save()
+            .then((response) => {
+                res.json({
+                    result: product,
+                    status: true,
+                    msg: "Product created successfully"
+                })
+            })
+            .catch((error)=> {
+                next({
+                    status: 400,
+                    msg: error
+                })
+            })
+
+        } catch(error){
+            next(error)
+        }
     }
 }
 module.exports = ProductController;
--- app/models/product.model.js
+++ app/models/product.model.js
@@ -1,6 +1,62 @@
 const mongoose = require("mongoose");
 const ProductSchemeDef  = new mongoose.Schema({
-    // TODO: Schema Defination
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Category'
+    },
+    image: [{
+        type: String
+    }],
+    price: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    description: {
+        type: String,
+    },
+    discount: {
+        type: Number,
+        default: null
+    },
+    after_discount: {
+        type: Number,
+        min: 1,
+        required: true
+    },
+    seller: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        default: null
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive', 'out-of-stock'],
+        default: 'inactive'
+    },
+    brands: {
+        type: mongoose.Types.ObjectId,
+        ref: "Label",
+        default: 'null'
+    },
+    is_feature: {
+        type: Boolean,
+        default: false
+    }
+
+}, {
+    timestamps: true,
+    autoCreate: true,
+    autoIndex: true
 });
 const ProductModel = mongoose.model("Product", ProductSchemeDef);
 module.exports = ProductModel;
